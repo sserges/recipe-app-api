b579ebd4777787cd529142c57ec8d23da0a54065
sserges <agounkeserge@gmail.com>
2019-06-10 16:34:26 +0000

    test_password_too_short updated

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -48,6 +48,7 @@ class PublicUserApiTests(TestCase):
             'password': 'pw'
         }
 
+        res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
