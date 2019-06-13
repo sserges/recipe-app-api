d3d1b81b77adc17d1240a411c713f8222c80d6a3
sserges <agounkeserge@gmail.com>
2019-06-13 18:55:55 +0000

    Added create tag feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -7,7 +7,9 @@ from core.models import Tag
 from recipe import serializers
 
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class TagViewSet(viewsets.GenericViewSet,
+                 mixins.ListModelMixin,
+                 mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
