bdb895d3e03c207a6954d4fc16c9cbedbe269798
sserges <agounkeserge@gmail.com>
2019-06-19 10:22:52 +0000

    test_models updated

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,7 +53,7 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
-    
+
     def test_ingredient_str(self):
         """Test the ingredient string representation"""
         ingredient = models.Ingredient.objects.create(
